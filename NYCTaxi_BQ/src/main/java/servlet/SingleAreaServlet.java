214adc0b9bcc419c9a5356a638cd3a2cd893dce5
Rocky  Luo <rocky.luo@rutgers.edu>
2016-11-23 02:47:30 -0500

    Update SingleAreaServlet.java

--- NYCTaxi_BQ/src/main/java/servlet/SingleAreaServlet.java
+++ NYCTaxi_BQ/src/main/java/servlet/SingleAreaServlet.java
@@ -20,10 +20,7 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-/**
- * Requests handler for estimating cost.
- * Created by YangYu on 2/13/16.
- */
+
 @WebServlet(name = "SingleAreaServlet")
 public class SingleAreaServlet extends HttpServlet {
 
